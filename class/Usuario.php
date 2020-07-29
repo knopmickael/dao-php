d8d58e1e0eb74a8c4954c3c4f762c586d25d4657
itakuio <knopmickael@gmail.com>
2020-07-29 01:29:29 -0300

    v.3.1

--- class/Usuario.php
+++ class/Usuario.php
@@ -38,12 +38,47 @@
                 ":ID"=>$id
             ));
 
+            $this->carregarDados($resultado);
+
+        }
+
+        public function carregarDados($resultado){
             if($resultado!=NULL){
                 $linha = $resultado[0];
                 $this->setUser_id($linha['user_id']);
                 $this->setUser_login($linha['user_login']);
                 $this->setUser_senha($linha['user_senha']);
+            } else {
+                throw new Exception("ERRO - USUÁRIO NÃO ENCONTRADO.");
             }
+        }
+
+        public static function getList(){
+            
+            $sql = new Sql();
+
+            return json_encode($sql->select("SELECT * FROM users ORDER BY user_id DESC"));
+
+        }
+
+        public static function search($pesquisa){
+            $sql = new Sql();
+
+            return json_encode($sql->select("SELECT * FROM users WHERE user_login LIKE :VALUE", array(
+                ":VALUE"=>"%".$pesquisa."%"
+            )));
+        }
+
+        public function logar($login, $senha){
+            
+            $sql = new Sql();
+
+            $resultado = $sql->select("SELECT * FROM users WHERE user_login = :LOGIN AND user_senha = :PASSWORD", array(
+                ":LOGIN"=>$login,
+                ":PASSWORD"=>$senha
+            ));
+
+            $this->carregarDados($resultado);
 
         }
 
